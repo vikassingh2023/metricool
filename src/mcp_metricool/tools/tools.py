0d2346e07f6b7555e0a2ebc7698898583c37e27f
Laura Esteban <lauraesteban@metricool.com>
2025-08-18 10:12:25 +0200

    Include carrousel tags for instagram posts

--- src/mcp_metricool/tools/tools.py
+++ src/mcp_metricool/tools/tools.py
@@ -655,8 +655,10 @@ async def update_schedule_post(id: str, date:str, blog_id: int, info: json) -> s
                                 Facebook type can be "POST", "REEL" or "STORY"
                                 Facebook title is only available for Facebook videos (This is separate from the main text of the post).
                                 Facebook boost, boostPayer, and boostBeneficiary are only included if they are promoted posts.
-                            "instagramData": {"type": "<string>" (default = POST), "collaborators":[{username: "string", deleted: false}]], "showReelOnFeed": "<boolean>" (default = true), "boost":<double>, "boostPayer":"<string>", "boostBeneficiary":"<string>"},
+                            "instagramData": {"type": "<string>" (default = POST), "collaborators":[{username: "string", deleted: false}]], "carouselTags":{"<number>":[{"username":"<string>","x":<double>,"y":<double>}]} (number is the index of the image within the carousel), "showReelOnFeed": "<boolean>" (default = true), "boost":<double>, "boostPayer":"<string>", "boostBeneficiary":"<string>"},
                                 Instagram type can be "POST", "REEL" or "STORY".
+                                There could be more than one collaborator, following the same structure.
+                                The carouselTags is a dictionary with the number of the image in the carousel as key and a list of tags for that image.
                             "linkedinData": {"documentTitle": "<string>", "publishImagesAsPDF": "<boolean>" (default = false), "previewIncluded": "<boolean>" (default = true), "type": "<string>" (default = post), "poll": {"question": "<string>", "options": [{"text": "<string>"}, {"text": "<string>"}], "settings": {"duration": "<string>"}}},
                                 Linkedin type can be "post" or "poll".
                                 If there is a documentTitle in Linkedin, publishImagesAsPDF must be true.
